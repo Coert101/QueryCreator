cd7ddd6d3501f1f1f0924c7d60a79ac939648c8f
RentonMcIntyre <rentonmcintyre@yahoo.com>
2017-04-12 10:50:00 +0200

    Added basic outline for parameter extraction
    Will now write parameter names and their SQL types to file. Does not
    handle inheritence.

--- QueryProj/QueryBuilder.py
+++ QueryProj/QueryBuilder.py
@@ -3,6 +3,20 @@ from os import listdir
 from os.path import isfile, join
 
 class QueryBuilder:
+    known_types = ["string", "int", "double", "decimal", "bool", "DateTime"]
+    type_equivalent = ["VARCHAR (255)", "INT", "DECIMAL", "DECIMAL", "BIT", "DATETIME"]
+
+
+
+    def extract_parameter(typeName, paramLine):
+        paramLine = paramLine.split(" ")
+
+        paramName = "["+paramLine[paramLine.index(typeName)+1]+"]"
+
+        typeName = QueryBuilder.type_equivalent[ QueryBuilder.known_types.index(typeName) ]
+
+        # how do we determine whether it is nullable?
+        return paramName + "\t\t" + typeName + "\t\tNOT NULL"
 
     def read_file(dir, dirTo, fileName):
 
@@ -11,6 +25,7 @@ class QueryBuilder:
 
         if os.path.exists(newName):
             os.remove(newName)
+
 
         for line in file:
             if (line.strip() != ""):
@@ -26,6 +41,17 @@ class QueryBuilder:
                                     tableName = tableName.split(' ', 2)[2]
                                     newFile = open(newName, "a")
                                     newFile.write(tableName)
+
+                            typeLine = next((typeName for typeName in QueryBuilder.known_types if typeName in line.split(" ")), False)
+                            # ignore calculated fields. We assume that any basic property will have the closing
+                            # bracket on the same line we further assume that a line using arrow notation (=>) is
+                            # also calculating
+                            if "}" not in line or "=>" in line:
+                                typeLine = False
+                            if typeLine is not False:
+                                newFile = open(newName, "a")
+                                newFile.write("\n"+QueryBuilder.extract_parameter(typeLine, line))
+
 
     def dir_builder(dirFrom, dirTo):
         for f in listdir(dirFrom) :
